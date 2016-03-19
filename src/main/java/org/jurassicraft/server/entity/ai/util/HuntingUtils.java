37307c62fa268f2f31972db8ae429e6a07228312
gegy1000 <gegy1000@gmail.com>
2016-03-19 11:13:41 +0200

    Revert "panorama (Not finished)"
    This reverts commit fd736b73dc439807f7ed2f802427b196192e91ba.

--- src/main/java/org/jurassicraft/server/entity/ai/util/HuntingUtils.java
+++ src/main/java/org/jurassicraft/server/entity/ai/util/HuntingUtils.java
@@ -97,7 +97,7 @@ public class HuntingUtils
     public static <K, V extends Comparable<? super V>> Map<K, V> sortByValue(Map<K, V> map)
     {
         List<Map.Entry<K, V>> list =
-                new LinkedList<Map.Entry<K, V>>(map.entrySet());
+                new LinkedList<>(map.entrySet());
         Collections.sort(list, new Comparator<Map.Entry<K, V>>()
         {
             public int compare(Map.Entry<K, V> o1, Map.Entry<K, V> o2)
@@ -106,7 +106,7 @@ public class HuntingUtils
             }
         });
 
-        Map<K, V> result = new LinkedHashMap<K, V>();
+        Map<K, V> result = new LinkedHashMap<>();
         for (Map.Entry<K, V> entry : list)
         {
             result.put(entry.getKey(), entry.getValue());
--- src/main/java/org/jurassicraft/server/proxy/ServerProxy.java
+++ src/main/java/org/jurassicraft/server/proxy/ServerProxy.java
@@ -23,7 +23,6 @@ import org.jurassicraft.server.block.JCBlockRegistry;
 import org.jurassicraft.server.configuration.JCConfigurations;
 import org.jurassicraft.server.creativetab.JCCreativeTabs;
 import org.jurassicraft.server.entity.base.JCEntityRegistry;
-import org.jurassicraft.server.event.MenuEvent;
 import org.jurassicraft.server.event.ServerEventHandler;
 import org.jurassicraft.server.handler.JCGuiHandler;
 import org.jurassicraft.server.item.JCItemRegistry;
@@ -64,11 +63,9 @@ public class ServerProxy
         NetworkRegistry.INSTANCE.registerGuiHandler(JurassiCraft.instance, new JCGuiHandler());
 
         ServerEventHandler eventHandler = new ServerEventHandler();
-        MenuEvent menuEvent = new MenuEvent();
 
         MinecraftForge.EVENT_BUS.register(JurassiCraft.configurations);
         MinecraftForge.EVENT_BUS.register(eventHandler);
-        MinecraftForge.EVENT_BUS.register(menuEvent);
     }
 
     protected void initContentHandlers(IContentHandler... contentHandlers)
