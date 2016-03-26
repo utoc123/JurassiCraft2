c00ef08d2c415e0527263f2adf1e80543041f456
gegy1000 <gegy1000@gmail.com>
2016-03-26 20:23:57 +0200

    Add Incubator egg rendering

--- src/main/java/org/jurassicraft/client/render/block/IncubatorSpecialRenderer.java
+++ src/main/java/org/jurassicraft/client/render/block/IncubatorSpecialRenderer.java
@@ -6,12 +6,15 @@ import net.minecraft.block.state.IBlockState;
 import net.minecraft.client.Minecraft;
 import net.minecraft.client.renderer.GlStateManager;
 import net.minecraft.client.renderer.tileentity.TileEntitySpecialRenderer;
+import net.minecraft.item.ItemStack;
 import net.minecraft.util.EnumFacing;
 import net.minecraft.util.ResourceLocation;
 import net.minecraft.world.World;
 import org.jurassicraft.JurassiCraft;
-import org.jurassicraft.server.block.BlockHandler;
+import org.jurassicraft.client.render.RenderingHandler;
+import org.jurassicraft.client.render.renderdef.RenderDinosaurDefinition;
 import org.jurassicraft.server.block.OrientedBlock;
+import org.jurassicraft.server.entity.base.EntityHandler;
 import org.jurassicraft.server.tileentity.IncubatorTile;
 import org.lwjgl.opengl.GL11;
 
@@ -39,43 +42,61 @@ public class IncubatorSpecialRenderer extends TileEntitySpecialRenderer<Incubato
     {
         World world = tileEntity.getWorld();
 
-        IBlockState blockState = world.getBlockState(tileEntity.getPos());
+        IBlockState state = world.getBlockState(tileEntity.getPos());
 
-        if (blockState.getBlock() == BlockHandler.INSTANCE.incubator)
+        GlStateManager.blendFunc(GL11.GL_SRC_ALPHA, GL11.GL_ONE_MINUS_SRC_ALPHA);
+        GlStateManager.enableBlend();
+        GlStateManager.disableCull();
+
+        EnumFacing value = state.getValue(OrientedBlock.FACING);
+
+        if (value == EnumFacing.NORTH || value == EnumFacing.SOUTH)
         {
-            GlStateManager.blendFunc(GL11.GL_SRC_ALPHA, GL11.GL_ONE_MINUS_SRC_ALPHA);
-            GlStateManager.enableBlend();
-            GlStateManager.disableCull();
+            value = value.getOpposite();
+        }
 
-            EnumFacing value = blockState.getValue(OrientedBlock.FACING);
+        int rotation = value.getHorizontalIndex() * 90;
 
-            if (value == EnumFacing.NORTH || value == EnumFacing.SOUTH)
-            {
-                value = value.getOpposite();
-            }
+        GlStateManager.pushMatrix();
 
-            int rotation = value.getHorizontalIndex() * 90;
+        GlStateManager.color(1.0F, 1.0F, 1.0F, 1.0F);
+        GlStateManager.translate(x + 0.5, y, z + 0.5);
 
+        GlStateManager.rotate(rotation, 0, 1, 0);
+
+        double scale = 0.5;
+        GlStateManager.scale(-scale, -scale, scale);
+
+        GlStateManager.translate(0, -1.5, 0);
+
+        mc.getTextureManager().bindTexture(texture);
+
+        model.render(null, 0, 0, 0, 0, 0, 0.0625F);
+
+        GlStateManager.popMatrix();
+
+        renderEgg(tileEntity.getStackInSlot(0), rotation, x, y, z, 0.2, 0.2);
+        renderEgg(tileEntity.getStackInSlot(1), rotation, x, y, z, 0.2, 0.8);
+        renderEgg(tileEntity.getStackInSlot(3), rotation, x, y, z, 0.8, 0.8);
+        renderEgg(tileEntity.getStackInSlot(4), rotation, x, y, z, 0.8, 0.2);
+        renderEgg(tileEntity.getStackInSlot(2), rotation, x, y + 0.05, z, 0.5, 0.5);
+
+        GlStateManager.disableBlend();
+        GlStateManager.enableCull();
+    }
+
+    public void renderEgg(ItemStack stack, float rotation, double x, double y, double z, double xOffset, double zOffset)
+    {
+        if (stack != null)
+        {
             GlStateManager.pushMatrix();
-
-            GlStateManager.color(1.0F, 1.0F, 1.0F, 1.0F);
-            GlStateManager.translate(x + 0.5, y, z + 0.5);
-
-            GlStateManager.rotate(rotation, 0, 1, 0);
-
-            double scale = 0.5;
-            GlStateManager.scale(-scale, -scale, scale);
-
-            GlStateManager.translate(0, -1.5, 0);
-
-            mc.getTextureManager().bindTexture(texture);
-
-            model.render(null, 0, 0, 0, 0, 0, 0.0625F);
-
+            GlStateManager.translate(x, y + 0.05, z);
+            GlStateManager.translate(xOffset, 1.2, zOffset);
+            GlStateManager.scale(-0.5F, -0.5F, -0.5F);
+            RenderDinosaurDefinition renderDef = RenderingHandler.INSTANCE.getRenderDef(EntityHandler.INSTANCE.getDinosaurById(stack.getItemDamage()));
+            mc.getTextureManager().bindTexture(renderDef.getEggTexture());
+            renderDef.getEggModel().render(null, 0.0F, 0.0F, 0.0F, 0.0F, 0.0F, 0.0625F);
             GlStateManager.popMatrix();
-
-            GlStateManager.disableBlend();
-            GlStateManager.enableCull();
         }
     }
 }
