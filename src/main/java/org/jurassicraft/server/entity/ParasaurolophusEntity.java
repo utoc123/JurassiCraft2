fd5356886c3151fef0135b9fe6d2294b24cc6068
gegy1000 <gegy1000@gmail.com>
2016-04-23 19:22:09 +0200

    Parasaurolophus sounds and calling

--- src/main/java/org/jurassicraft/server/entity/ParasaurolophusEntity.java
+++ src/main/java/org/jurassicraft/server/entity/ParasaurolophusEntity.java
@@ -1,8 +1,6 @@
 package org.jurassicraft.server.entity;
 
 import net.minecraft.world.World;
-import org.jurassicraft.client.animation.Animations;
-import org.jurassicraft.server.entity.ai.animations.JCNonAutoAnimBase;
 import org.jurassicraft.server.entity.base.DefensiveDinosaurEntity;
 
 public class ParasaurolophusEntity extends DefensiveDinosaurEntity // implements IEntityAICreature, IHerbivore
@@ -10,6 +8,5 @@ public class ParasaurolophusEntity extends DefensiveDinosaurEntity // implements
     public ParasaurolophusEntity(World world)
     {
         super(world);
-        animationTasks.addTask(2, new JCNonAutoAnimBase(this, Animations.CALLING.get(), 100)); // Call
     }
 }
