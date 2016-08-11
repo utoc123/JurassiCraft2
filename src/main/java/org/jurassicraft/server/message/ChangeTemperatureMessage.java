dd57edbf7e6f1527050ebd9386d4c5dddb62ada0
gegy1000 <gegy1000@gmail.com>
2016-08-11 07:36:01 +0200

    Fix ChangeTemperatureMessage crashing server when client sends invalid packet

--- src/main/java/org/jurassicraft/server/message/ChangeTemperatureMessage.java
+++ src/main/java/org/jurassicraft/server/message/ChangeTemperatureMessage.java
@@ -6,16 +6,15 @@ import net.minecraft.client.Minecraft;
 import net.minecraft.entity.player.EntityPlayer;
 import net.minecraft.inventory.IInventory;
 import net.minecraft.server.MinecraftServer;
+import net.minecraft.tileentity.TileEntity;
 import net.minecraft.util.math.BlockPos;
+import net.minecraftforge.fml.common.network.ByteBufUtils;
 import net.minecraftforge.fml.common.network.simpleimpl.MessageContext;
 import org.jurassicraft.JurassiCraft;
 
 public class ChangeTemperatureMessage extends AbstractMessage<ChangeTemperatureMessage> {
     private int slot;
     private int temp;
-    private int x;
-    private int y;
-    private int z;
     private BlockPos pos;
 
     public ChangeTemperatureMessage() {
@@ -24,42 +23,39 @@ public class ChangeTemperatureMessage extends AbstractMessage<ChangeTemperatureM
     public ChangeTemperatureMessage(BlockPos pos, int slot, int temp) {
         this.slot = slot;
         this.temp = temp;
-        this.x = pos.getX();
-        this.y = pos.getY();
-        this.z = pos.getZ();
-        this.pos = new BlockPos(this.x, this.y, this.z);
+        this.pos = pos;
     }
 
     @Override
     public void onClientReceived(Minecraft minecraft, ChangeTemperatureMessage message, EntityPlayer player, MessageContext messageContext) {
-        IInventory incubator = (IInventory) player.worldObj.getTileEntity(message.pos);
-        incubator.setField(message.slot + 10, message.temp);
+        TileEntity tile = player.worldObj.getTileEntity(message.pos);
+        if (tile instanceof IInventory) {
+            IInventory incubator = (IInventory) tile;
+            incubator.setField(message.slot + 10, message.temp);
+        }
     }
 
     @Override
     public void onServerReceived(MinecraftServer server, ChangeTemperatureMessage message, EntityPlayer player, MessageContext messageContext) {
-        IInventory incubator = (IInventory) player.worldObj.getTileEntity(message.pos);
-        incubator.setField(message.slot + 10, message.temp);
-        JurassiCraft.NETWORK_WRAPPER.sendToAll(new ChangeTemperatureMessage(message.pos, message.slot, message.temp));
+        TileEntity tile = player.worldObj.getTileEntity(message.pos);
+        if (tile instanceof IInventory) {
+            IInventory incubator = (IInventory) tile;
+            incubator.setField(message.slot + 10, message.temp);
+            JurassiCraft.NETWORK_WRAPPER.sendToAll(new ChangeTemperatureMessage(message.pos, message.slot, message.temp));
+        }
     }
 
     @Override
     public void toBytes(ByteBuf buffer) {
         buffer.writeInt(this.slot);
         buffer.writeInt(this.temp);
-        buffer.writeInt(this.x);
-        buffer.writeInt(this.y);
-        buffer.writeInt(this.z);
+        buffer.writeLong(this.pos.toLong());
     }
 
     @Override
     public void fromBytes(ByteBuf buffer) {
         this.slot = buffer.readInt();
         this.temp = buffer.readInt();
-        this.x = buffer.readInt();
-        this.y = buffer.readInt();
-        this.z = buffer.readInt();
-
-        this.pos = new BlockPos(this.x, this.y, this.z);
+        this.pos = BlockPos.fromLong(buffer.readLong());
     }
 }
