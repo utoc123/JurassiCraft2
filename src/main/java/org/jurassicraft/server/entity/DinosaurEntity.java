e41a2a7edb9e5a69c058969ca7844566af4bd36b
gegy1000 <gegy1000@gmail.com>
2016-09-01 13:26:05 +0200

    Fix dinosaurs glitching through walls after relog or chunk reload

--- src/main/java/org/jurassicraft/server/entity/DinosaurEntity.java
+++ src/main/java/org/jurassicraft/server/entity/DinosaurEntity.java
@@ -122,6 +122,8 @@ public abstract class DinosaurEntity extends EntityCreature implements IEntityAd
     private boolean useInertialTweens;
     private List<Class<? extends EntityLivingBase>> attackTargets = new ArrayList<>();
 
+    private boolean deserializing;
+
     private int attackCooldown;
 
     @SideOnly(Side.CLIENT)
@@ -491,8 +493,6 @@ public abstract class DinosaurEntity extends EntityCreature implements IEntityAd
         this.dinosaur = EntityHandler.getDinosaurByClass(this.getClass());
 
         this.getAttributeMap().registerAttribute(SharedMonsterAttributes.ATTACK_DAMAGE);
-        this.updateAttributes();
-        this.updateBounds();
     }
 
     public void updateAttributes() {
@@ -501,15 +501,9 @@ public abstract class DinosaurEntity extends EntityCreature implements IEntityAd
 
         this.getEntityAttribute(SharedMonsterAttributes.MAX_HEALTH).setBaseValue(newHealth);
         this.getEntityAttribute(SharedMonsterAttributes.MOVEMENT_SPEED).setBaseValue(this.interpolate(this.dinosaur.getBabySpeed(), this.dinosaur.getAdultSpeed()));
-//        getEntityAttribute(SharedMonsterAttributes.knockbackResistance).setBaseValue(interpolate(dinosaur.getBabyKnockback(), dinosaur.getAdultKnockback())); TODO
-
-        // updateBounds();
 
         this.getEntityAttribute(SharedMonsterAttributes.ATTACK_DAMAGE).setBaseValue(this.interpolate(this.dinosaur.getBabyStrength(), this.dinosaur.getAdultStrength()));
 
-        // EntityLiving has a base of 16 the AI needs to have longer range for things like Herding
-        // DO NOT CHANGE FOR NOW - Eventually we'll make the AI work in smaller increments and probably
-        //                         have different ranges for different eyesights, but for now please keep it long.
         this.getEntityAttribute(SharedMonsterAttributes.FOLLOW_RANGE).setBaseValue(64.0D);
 
         if (prevHealth != newHealth) {
@@ -1053,6 +1047,8 @@ public abstract class DinosaurEntity extends EntityCreature implements IEntityAd
 
     @Override
     public void readFromNBT(NBTTagCompound nbt) {
+        this.deserializing = true;
+
         super.readFromNBT(nbt);
         this.setAge(nbt.getInteger("DinosaurAge"));
         this.setCarcass(nbt.getBoolean("IsCarcass"));
@@ -1077,6 +1073,8 @@ public abstract class DinosaurEntity extends EntityCreature implements IEntityAd
 
         this.updateAttributes();
         this.updateBounds();
+
+        this.deserializing = false;
     }
 
     @Override
@@ -1230,15 +1228,17 @@ public abstract class DinosaurEntity extends EntityCreature implements IEntityAd
     protected void setSize(float width, float height) {
         if (width != this.width || height != this.height) {
             float prevWidth = this.width;
-
             this.width = width;
             this.height = height;
-
-            AxisAlignedBB bounds = this.getEntityBoundingBox();
-            this.setEntityBoundingBox(new AxisAlignedBB(bounds.minX, bounds.minY, bounds.minZ, bounds.minX + (double) this.width, bounds.minY + (double) this.height, bounds.minZ + (double) this.width));
-
-            if (this.width > prevWidth && !this.firstUpdate && !this.worldObj.isRemote) {
-                this.moveEntity(prevWidth - this.width, 0.0, prevWidth - this.width);
+            if (!this.deserializing) {
+                AxisAlignedBB bounds = this.getEntityBoundingBox();
+                this.setEntityBoundingBox(new AxisAlignedBB(bounds.minX, bounds.minY, bounds.minZ, bounds.minX + this.width, bounds.minY + this.height, bounds.minZ + this.width));
+                if (this.width > prevWidth && !this.firstUpdate && !this.worldObj.isRemote) {
+                    this.moveEntity(prevWidth - this.width, 0.0F, prevWidth - this.width);
+                }
+            } else {
+                float halfWidth = this.width / 2.0F;
+                this.setEntityBoundingBox(new AxisAlignedBB(this.posX - halfWidth, this.posY, this.posZ - halfWidth, this.posX + halfWidth, this.posY + this.height, this.posZ + halfWidth));
             }
         }
     }
